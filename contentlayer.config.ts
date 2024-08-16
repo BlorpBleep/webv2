7f689a98acc7ed43160302d49369095dd9903f74
0xCosydell <david.awatere@gmail.com>
2024-08-16 12:40:47 +0200

    .

---
+++ contentlayer.config.ts
@@ -0,0 +1,12 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.md`,
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+  },
+}))
+
+export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] })
