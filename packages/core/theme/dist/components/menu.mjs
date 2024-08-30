4547480cb096ca0cde35e97f7bf0bfdf4feec331
Catherine Awatere <153932808+iOS-Cathy@users.noreply.github.com>
2024-08-30 13:29:43 +0200

    .

--- packages/core/theme/dist/components/menu.mjs
+++ packages/core/theme/dist/components/menu.mjs
@@ -4,8 +4,8 @@ import {
   menuSection
 } from "../chunk-4YQYZDME.mjs";
 import "../chunk-IV3K5WDK.mjs";
-import "../chunk-2PIR7DFM.mjs";
 import "../chunk-3XVMTUU7.mjs";
+import "../chunk-2PIR7DFM.mjs";
 import "../chunk-N2KXC5ZE.mjs";
 import "../chunk-BYWORP66.mjs";
 import "../chunk-XHQUSKIE.mjs";
