d0d6ad404ab92680f8b3faac205116213c55cdcf
0xCosydell <david.awatere@gmail.com>
2024-09-01 12:27:16 +0200

    whew!

--- apps/docs/components/navbar.tsx
+++ apps/docs/components/navbar.tsx
@@ -1,160 +1,289 @@
 "use client";
 
+import { useRef, useState, FC, ReactNode, Key, useEffect } from "react";
+import {
+  link,
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  Link,
+  Button,
+  Kbd,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownTrigger,
+  Chip,
+} from "@nextui-org/react";
+import { clsx } from "@nextui-org/shared-utils";
 import NextLink from "next/link";
-import { Button, Chip } from "@nextui-org/react";
-import { ArrowRightIcon } from "@nextui-org/shared-icons";
-import dynamic from "next/dynamic";
-import { title, subtitle } from "@/components/primitives";
+import { usePathname } from "next/navigation";
+import { includes } from "lodash";
+import { ArrowRightIcon, SearchLinearIcon } from "@nextui-org/shared-icons";
+import { isAppleDevice } from "@react-aria/utils";
+import { usePress } from "@react-aria/interactions";
+import { useFocusRing } from "@react-aria/focus";
+
+import { currentVersion } from "@/utils/version";
+import { siteConfig } from "@/config/site";
+import { Route } from "@/libs/docs/page";
+import { LargeLogo, SmallLogo, ThemeSwitch } from "@/components";
+import { useIsMounted } from "@/hooks/use-is-mounted";
+import { DocsSidebar } from "@/components/docs/sidebar";
+import { useCmdkStore } from "@/components/cmdk";
 import { trackEvent } from "@/utils/va";
-import Image from "next/image";
-import { FaWindows, FaAmazon, FaAndroid, FaApple, FaGamepad, FaChrome, FaFirefox, FaLinux, FaTv, FaShieldAlt } from 'react-icons/fa';
-import { SiAppletv, SiAndroid } from 'react-icons/si';
-import { MdOutlineDesktopMac } from 'react-icons/md';
 
-const BgLooper = dynamic(() => import("./bg-looper").then((mod) => mod.BgLooper), {
-  ssr: false,
-});
+export interface NavbarProps {
+  routes: Route[];
+  mobileRoutes?: Route[];
+  tag?: string;
+  slug?: string;
+  children?: ReactNode;
+}
 
-export const Hero = () => {
-  const handlePressAnnouncement = (name: string, url: string) => {
+export const Navbar: FC<NavbarProps> = ({ children, routes, mobileRoutes = [], slug, tag }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [commandKey, setCommandKey] = useState<"ctrl" | "command">("command");
+
+  const ref = useRef<HTMLElement>(null);
+  const isMounted = useIsMounted();
+
+  const pathname = usePathname();
+
+  const cmdkStore = useCmdkStore();
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    setCommandKey(isAppleDevice() ? "command" : "ctrl");
+  }, []);
+
+  const handleOpenCmdk = () => {
+    cmdkStore.onOpen();
+    trackEvent("Navbar - Search", {
+      name: "navbar - search",
+      action: "press",
+      category: "cmdk",
+    });
+  };
+
+  const { pressProps } = usePress({
+    onPress: handleOpenCmdk,
+  });
+  const { focusProps, isFocusVisible } = useFocusRing();
+
+  const searchButton = (
+    <Button
+      aria-label="Quick search"
+      className="text-sm font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20"
+      endContent={
+        <Kbd className="hidden py-0.5 px-2 lg:inline-block" keys={commandKey}>
+          K
+        </Kbd>
+      }
+      startContent={
+        <SearchLinearIcon
+          className="text-base text-default-400 pointer-events-none flex-shrink-0"
+          size={18}
+          strokeWidth={2}
+        />
+      }
+      onPress={handleOpenCmdk}
+    >
+      Quick Search...
+    </Button>
+  );
+
+  if (pathname.includes("/examples")) {
+    return null;
+  }
+
+  const navLinkClasses = clsx(link({ color: "foreground" }), "data-[active=true]:text-primary");
+
+  const handleVersionChange = (key: Key) => {
+    if (key === "v1") {
+      const newWindow = window.open("https://v1.nextui.org", "_blank", "noopener,noreferrer");
+      if (newWindow) newWindow.opener = null;
+    }
+  };
+
+  const handlePressNavbarItem = (name: string, url: string) => {
     trackEvent("NavbarItem", {
       name,
       action: "press",
-      category: "home - hero",
+      category: "navbar",
       data: url,
     });
   };
 
   return (
     <>
-      <section className="flex relative overflow-hidden lg:overflow-visible w-full flex-nowrap justify-between items-center h-[calc(60vh_-_64px)] 2xl:h-[calc(64vh_-_44px)]">
-        <div className="flex relative z-20 flex-col gap-6 w-full lg:w-1/2 xl:mt-10">
-          <div className="w-full flex justify-center md:hidden">
-            <Chip
-              as={NextLink}
-              className="bg-default-100/50 hover:bg-default-100 border-default-200/80 dark:border-default-100/80 transition-colors cursor-pointer"
-              color="default"
-              href="/blog/v2.3.0"
-              variant="dot"
-              onClick={() => handlePressAnnouncement("New version v2.4.0", "/blog/v2.4.0")}
+      <NextUINavbar
+        ref={ref}
+        className={clsx({
+          "z-[100001]": isMenuOpen,
+          "bg-transparent": true,
+          "hidden md:flex": true, // Hide navbar on small screens and show on medium and larger
+        })}
+        isMenuOpen={isMenuOpen}
+        maxWidth="full"
+        position="sticky"
+        onMenuOpenChange={setIsMenuOpen}
+      >
+        {/* Left section: Logo */}
+        <NavbarContent className="basis-1/4 sm:basis-1/4 bg-transparent" justify="start">
+          <NavbarBrand as="li" className="gap-3">
+            <NextLink
+              aria-label="Home"
+              className="flex justify-start items-center gap-2 tap-highlight-transparent transition-opacity active:opacity-50"
+              href="/"
+              onClick={() => handlePressNavbarItem("Home", "/")}
             >
-              New version v2.4.0&nbsp;
-              <span aria-label="emoji" role="img">
-                🚀
-              </span>
-            </Chip>
-          </div>
+              <SmallLogo className="w-6 h-6 md:hidden" />
+              <LargeLogo className="h-5 md:h-6" />
+            </NextLink>
+          </NavbarBrand>
+        </NavbarContent>
 
-          <div className="text-center leading-8 md:leading-10 md:text-left mt-5 md:mt-0">
-            <div className="inline-block">
-              <h1 className={title()}>Get 63% off&nbsp;</h1>
-              <h1 className={title({ color: "violet" })}>CicadaVPN </h1>
-            </div>
-            <h1 className={title({ color: "blue" })}> + 3 months free for a friend</h1>
-          </div>
-          <h2 className={subtitle({ fullWidth: true, class: "text-center md:text-left" })}>
-            Choose a 2-year plan, gift your friend 3 months CicadaVPN protection free.
-          </h2>
-          <div className="flex flex-col md:flex-row items-center gap-4 mb-6 md:mb-0">
-            <Button
-              as={NextLink}
-              className="w-full md:h-11 md:w-auto font-bold"
-              color="primary"
-              endContent={
-                <ArrowRightIcon
-                  className="group-data-[hover=true]:translate-x-0.5 outline-none transition-transform"
-                  strokeWidth={2}
-                />
-              }
-              href="/pricing"
-              radius="full"
-              size="lg"
-              onPress={() => {
-                trackEvent("Hero - Get Started", {
-                  name: "Get Started",
-                  action: "click",
-                  category: "landing-page",
-                  data: "/pricing",
-                });
-              }}
-            >
-              Get the Deal | 82% Off
-            </Button>
-          </div>
-          <div className="flex items-center justify-center md:justify-start text-lg mt-2">
-            <FaShieldAlt className="text-primary-500 mr-2" />
-            <span className="text-gray-700 dark:text-gray-300">45-Day Money-Back Guarantee</span>
-          </div>
-        </div>
+        {/* Middle section: Navigation Links */}
+        <NavbarContent className="basis-1/2 justify-center bg-transparent items-center" justify="center">
+          <ul className="flex gap-4 justify-center items-center">
+            <NavbarItem>
+              <NextLink
+                className={navLinkClasses}
+                color="foreground"
+                data-active={includes(pathname, "pricing")}
+                href="/pricing"
+                onClick={() => handlePressNavbarItem("Pricing", "/pricing")}
+              >
+                Pricing
+              </NextLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NextLink
+                className={navLinkClasses}
+                color="foreground"
+                data-active={includes(pathname, "downloads")}
+                href="/downloads"
+                onClick={() => handlePressNavbarItem("Downloads", "/downloads")}
+              >
+                Download
+              </NextLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NextLink
+                className={navLinkClasses}
+                color="foreground"
+                data-active={includes(pathname, "blog")}
+                href="/blog"
+                onClick={() => handlePressNavbarItem("Blog", "/blog")}
+              >
+                Blog
+              </NextLink>
+            </NavbarItem>
+            <NavbarItem>
+              <NextLink
+                className={navLinkClasses}
+                color="foreground"
+                data-active={includes(pathname, "teams")}
+                href="/teams"
+                onClick={() => handlePressNavbarItem("Teams", "/teams")}
+              >
+                Teams
+              </NextLink>
+            </NavbarItem>
 
-        {/* Right-hand side image, hidden on small screens */}
-        <div className="relative z-10 w-full lg:w-1/2 flex justify-center items-center hidden md:flex">
-          <Image
-            src="/images/cicada.png"
-            alt="CicadaVPN"
-            width={400}
-            height={400}
-            className="object-contain"
+            <NavbarItem>
+              <NextLink
+                className={navLinkClasses}
+                color="foreground"
+                data-active={includes(pathname, "help")}
+                href="/docs/faq/plans_orders"
+                onClick={() => handlePressNavbarItem("Help", "/docs/faq/plans_orders")}
+              >
+                Help
+              </NextLink>
+            </NavbarItem>
+          </ul>
+        </NavbarContent>
+
+        {/* Right section: My Account Link, Get CicadaVPN Button, and Theme Switch */}
+        <NavbarContent className="basis-1/4 flex gap-4 bg-transparent items-center justify-end">
+          <NavbarItem className="flex h-full items-center ml-auto">
+            <ThemeSwitch className="flex items-center" />
+          </NavbarItem>
+
+          <NavbarItem className="flex h-full items-center">
+            <NextLink href="/account" passHref legacyBehavior>
+              <Link
+                className={clsx(navLinkClasses, "flex items-center")}
+                color="foreground"
+                href="/account"
+                onClick={() => handlePressNavbarItem("Account", "/account")}
+              >
+                My Account
+              </Link>
+            </NextLink>
+          </NavbarItem>
+
+          {/* Button: Hidden on small screens, visible on medium and larger */}
+          <NavbarItem className="hidden md:flex h-full items-center">
+            <NextLink href="/pricing" passHref legacyBehavior>
+              <Button
+                as={NextLink}
+                className="w-full md:h-11 md:w-auto font-bold flex items-center"
+                color="primary"
+                endContent={
+                  <ArrowRightIcon
+                    className="group-data-[hover=true]:translate-x-0.5 outline-none transition-transform"
+                    strokeWidth={2}
+                  />
+                }
+                href="/pricing"
+                radius="full"
+                size="lg"
+                onPress={() => {
+                  trackEvent("Navbar - Get CicadaVPN", {
+                    name: "Get CicadaVPN",
+                    action: "click",
+                    category: "navbar",
+                    data: "/pricing",
+                  });
+                }}
+              >
+                Get 82% Off
+              </Button>
+            </NextLink>
+          </NavbarItem>
+
+          {/* Menu toggle: Visible on small screens, hidden on medium and larger */}
+          <NavbarItem className="w-10 h-full flex md:hidden">
+            <NavbarMenuToggle
+              aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+              className="w-full h-full pt-1"
+              onClick={() => setIsMenuOpen(!isMenuOpen)} // Explicitly handle menu toggle
+            />
+          </NavbarItem>
+        </NavbarContent>
+
+        {/* Ensure the menu is correctly toggled */}
+        <NavbarMenu className="md:hidden">
+          <DocsSidebar
+            className="mt-4 pt-8"
+            routes={[...mobileRoutes, ...routes]}
+            slug={slug}
+            tag={tag}
           />
-        </div>
-
-        <BgLooper />
-      </section>
-
-      {/* Supported Platforms Section */}
-      <section className="flex flex-col items-center pb-8 -mt-12">
-        <h3 className="text-xl font-semibold text-gray-700 dark:text-gray-300">Supported on:</h3>
-        <div className="grid grid-cols-4 gap-6 mt-4 md:grid-cols-6 lg:flex lg:flex-wrap lg:justify-center lg:gap-6">
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaWindows className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Windows</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaAmazon className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Amazon Fire TV</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaAndroid className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Android</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <SiAndroid className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Android TV</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <SiAppletv className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Apple TV</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaGamepad className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Console</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaChrome className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Chrome</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaFirefox className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Firefox</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaApple className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">iOS</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaLinux className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Linux</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <MdOutlineDesktopMac className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">macOS</span>
-          </div>
-          <div className="flex flex-col items-center text-center text-gray-500 dark:text-gray-400">
-            <FaTv className="w-8 h-8 md:w-10 md:h-10" />
-            <span className="mt-2 text-xs md:text-sm">Smart TVs</span>
-          </div>
-        </div>
-      </section>
+          {children}
+        </NavbarMenu>
+      </NextUINavbar>
     </>
   );
 };
