ae195a57adb432dca6bdb6b0d63ba517d0df481e
0xCosydell <david.awatere@gmail.com>
2024-09-07 20:45:55 +0200

    Sitemap generation, new pricing page offer boxes, cute lil cicads

--- apps/docs/app/pricing/page.tsx
+++ apps/docs/app/pricing/page.tsx
@@ -1,26 +1,8 @@
-"use client";
+"use client"; // Ensure client-side rendering
 
 import React, { useEffect, useState } from "react";
-import { AiOutlineGlobal } from "react-icons/ai";
-import { title, subtitle } from "@/components/primitives";
-
-// Function to calculate time remaining
-const calculateTimeLeft = (expiryTime: Date) => {
-  const difference = +new Date(expiryTime) - +new Date();
-  let timeLeft = {
-    minutes: "00",
-    seconds: "00",
-  };
-
-  if (difference > 0) {
-    timeLeft = {
-      minutes: Math.floor((difference / 1000 / 60) % 60).toString().padStart(2, "0"),
-      seconds: Math.floor((difference / 1000) % 60).toString().padStart(2, "0"),
-    };
-  }
-
-  return timeLeft;
-};
+import { Card, CardHeader, CardBody, Image, Tooltip, Badge, Accordion, AccordionItem } from "@nextui-org/react";
+import { title } from "@/components/primitives";
 
 export default function PricingPage() {
   const [timeLeft, setTimeLeft] = useState({
@@ -33,106 +15,158 @@ export default function PricingPage() {
     jwtExpiryTime.setMinutes(jwtExpiryTime.getMinutes() + 60); // Set expiry time to 60 minutes from now
 
     const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft(jwtExpiryTime));
+      setTimeLeft({
+        minutes: Math.floor((+new Date(jwtExpiryTime) - +new Date()) / 1000 / 60).toString().padStart(2, "0"),
+        seconds: Math.floor((+new Date(jwtExpiryTime) - +new Date()) / 1000 % 60).toString().padStart(2, "0"),
+      });
     }, 1000);
 
     return () => clearInterval(timer);
   }, []);
 
+  const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
   return (
-    <>
-      <main className="relative container mx-auto w-full sm:w-[90%] lg:w-[70%] z-10 px-4 sm:px-6 lg:px-8 min-h-[calc(100vh_-_64px_-_108px)] mb-12 flex-grow bg-white dark:bg-black pt-8">
-        <section className="w-full flex flex-col items-center mt-4 sm:mt-6 gap-6 sm:gap-8 mx-auto">
-          <div className="text-center w-full mb-4 sm:mb-8">
-            <div className="text-center leading-8 md:leading-10 ">
-              <h1 className={title({ color: "violet", size: "lg" })}>Get 83% off</h1>
-              <div>
-                <h1 className={title({ size: "lg" })}>CicadaVPN&nbsp;</h1>
-                <h1 className={title({ color: "green", size: "lg" })}>+ 3 Months Free</h1>
-              </div>
-            </div>
-
-            {/* Countdown Timer */}
-            <h2 className="text-xl sm:text-2xl">
-              <span className="text-black dark:text-white">Offer Ends In </span>
-              <span className="text-red-600 dark:text-red-400">
-                {`${timeLeft.minutes}Min:${timeLeft.seconds}Sec`}
-              </span>
-            </h2>
-          </div>
-
-          {/* Offer Boxes */}
-          <div className="flex flex-wrap md:flex-nowrap justify-center gap-6 items-start w-full">
-            {/* Standard Offer - Left with gradient */}
-            <div className="order-2 md:order-1 border border-gray-300 dark:border-gray-600 p-6 rounded-lg bg-gradient-to-br from-blue-200 to-blue-400 dark:from-blue-700 dark:to-blue-900 flex-grow flex-shrink flex-basis-[30%] flex flex-col justify-between shadow-md sm:min-h-[300px] min-h-[300px] sm:self-center">
-              <AiOutlineGlobal size={40} className="text-gray-600 dark:text-gray-400 mb-4" />
-              <h3 className="text-2xl font-bold text-black dark:text-white">
-                Cicada VPN - 1 Month Plan
-              </h3>
-              <p className="text-xl text-gray-700 dark:text-gray-400 mt-4 flex-grow">
-                11.99 €/mo for 1 year of secure VPN access.
-              </p>
-              <a
-                href="https://buy.stripe.com/5kA28K1HobDL5WgbIP"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-4 rounded-lg text-lg flex items-center mt-6 w-[60%] mx-auto justify-center"
-              >
-                Checkout →
-              </a>
-            </div>
-
-            {/* Best Value Offer - First on small screens, middle on larger */}
-            <div className="order-1 md:order-2 border border-green-600 p-6 rounded-lg bg-gradient-to-br from-green-500 to-green-700 flex-grow flex-shrink flex-basis-[30%] flex flex-col justify-between shadow-lg relative sm:min-h-[400px] min-h-[300px] sm:self-center">
-              <div className="absolute top-0 left-0 bg-green-700 text-white px-4 py-1 text-xs font-bold rounded-tl-lg rounded-br-lg">
-                Best Value
-              </div>
-              <AiOutlineGlobal size={50} className="text-white mb-6" />
-              <h3 className="text-3xl font-bold text-white">
-                Cicada VPN - 2 Years + 4 Months FREE
-              </h3>
-              <p className="text-2xl text-gray-200 mt-4 flex-grow">
-                2.03 €/mo for enhanced privacy and global access.
-              </p>
-              <a
-                href="https://buy.stripe.com/3cs3cOgCi0Z784obIN"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-white hover:bg-gray-100 text-green-700 font-bold py-4 rounded-lg text-lg flex items-center mt-6 w-[60%] mx-auto justify-center"
-              >
-                Checkout →
-              </a>
-            </div>
-
-            {/* Basic Offer - Right with gradient */}
-            <div className="order-3 md:order-3 border border-gray-300 dark:border-gray-600 p-6 rounded-lg bg-gradient-to-bl from-blue-200 to-blue-400 dark:from-blue-700 dark:to-blue-900 flex-grow flex-shrink flex-basis-[30%] flex flex-col justify-between shadow-md sm:min-h-[300px] min-h-[300px] sm:self-center">
-              <AiOutlineGlobal size={40} className="text-gray-600 dark:text-gray-400 mb-4" />
-              <h3 className="text-2xl font-bold text-black dark:text-white">
-                Cicada VPN - 6 Months Plan
-              </h3>
-              <p className="text-xl text-gray-700 dark:text-gray-400 mt-4 flex-grow">
-                6.99 €/mo for 6 months of VPN protection.
-              </p>
-              <a
-                href="https://buy.stripe.com/6oE7t485MgY5doI3ci"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-4 rounded-lg text-lg flex items-center mt-6 w-[60%] mx-auto justify-center"
-              >
-                Checkout →
-              </a>
-            </div>
-          </div>
-
-          <div className="text-center text-gray-500 dark:text-gray-400 mt-10 text-sm">
-            *All amounts shown are in Euros. Taxes may apply based on your jurisdiction.
-          </div>
-        </section>
-
-        <div className="text-center text-black dark:text-white mt-12 text-2xl font-bold">
-          See What’s Included in All Plans
+    <main className="relative container mx-auto w-full sm:w-[90%] lg:w-[70%] z-10 px-4 sm:px-6 lg:px-8 min-h-[calc(100vh_-_64px_-_108px)] mb-12 flex-grow bg-white dark:bg-black pt-8">
+      {/* Promotional Section */}
+      <div className="flex flex-col justify-center items-center text-center leading-8 md:leading-10 mb-8 h-40">
+        <h1 className={title({ color: "violet", size: "lg" })}>Get 83% Off</h1>
+        <div>
+          <h1 className={title({ size: "lg" })}>CicadaVPN&nbsp;</h1>
+          <h1 className={title({ color: "green", size: "lg" })}>+ 4 Months Free</h1>
         </div>
-      </main>
-    </>
+
+        {/* Countdown Timer */}
+        <h1 className="text-xl sm:text-2xl mt-2">
+          <span className={title({ color: "pink", size: "sm" })}>Offer Ends In </span>
+          <span className={title({ color: "blue", size: "sm" })}>
+            {`${timeLeft.minutes}Min:${timeLeft.seconds}Sec`}
+          </span>
+        </h1>
+      </div>
+
+      <section className="w-full flex flex-col lg:flex-row lg:justify-between mt-4 sm:mt-6 gap-6 sm:gap-8 mx-auto">
+
+        {/* Best Value Offer (Order: First on mobile, middle on desktop screens, 10% bigger, with badge and border) */}
+        <Card 
+          className="py-4 shadow-lg bg-white dark:bg-gray-800 rounded-lg w-full lg:w-[33%] lg:h-[500px] border-2 border-gradient-to-r from-[#F54180] to-[#338EF7] cursor-pointer relative order-1 lg:order-2"
+          as="a" href="https://buy.stripe.com/3cs3cOgCi0Z784obIN"
+        >
+          {/* Badge */}
+          <Badge 
+            className="absolute top-3 right-3 z-50 bg-blue-600 text-white rounded-full px-4 py-2"
+            style={{ fontSize: '1rem', zIndex: 10 }}
+          >
+            Most Popular
+          </Badge>
+
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <p className="text-tiny uppercase font-bold text-gray-900 dark:text-gray-200">Best Value</p>
+            <small className="text-default-500 dark:text-gray-400">+ 4 Extra Months Free</small>
+            <h4 className="font-bold text-large text-gray-900 dark:text-gray-200">Cicada VPN - 2 Years Plan</h4>
+            <p className="text-default-500 text-sm mt-2 dark:text-gray-400">2.03 €/mo for enhanced privacy and global access.</p>
+          </CardHeader>
+
+          {/* Image Section with More Info */}
+          <CardBody className="relative py-2 px-4">
+            <Image
+              alt="Best Value Offer"
+              className="object-cover rounded-xl"
+              src="/images/first.webp"
+              width="100%"
+            />
+            {/* Transparent Overlay */}
+            <div className="absolute bottom-0 left-0 right-0 bg-white/20 dark:bg-black/20 backdrop-blur-md rounded-b-xl py-4 px-4 flex justify-between items-center">
+              <Tooltip content="Best Deal" placement="top" color="foreground">
+                <p className="text-tiny text-default-500 cursor-pointer">More Info</p>
+              </Tooltip>
+            </div>
+          </CardBody>
+        </Card>
+
+        {/* First Card (Standard Offer) */}
+        <Card 
+          className="py-4 shadow-lg bg-white dark:bg-gray-800 rounded-lg w-full lg:w-[30%] lg:h-[450px] cursor-pointer lg:order-1"
+          as="a" href="https://buy.stripe.com/5kA28K1HobDL5WgbIP"
+        >
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <p className="text-tiny uppercase font-bold text-gray-900 dark:text-gray-200">Special Offer</p>
+            <small className="text-default-500 dark:text-gray-400">3 Months Free</small>
+            <h4 className="font-bold text-large text-gray-900 dark:text-gray-200">Cicada VPN - 1 Month Plan</h4>
+            <p className="text-default-500 text-sm mt-2 dark:text-gray-400">11.99 €/mo for 1 year of secure VPN access.</p>
+          </CardHeader>
+
+          {/* Image Section with More Info */}
+          <CardBody className="relative py-2 px-4">
+            <Image
+              alt="Standard Offer"
+              className="object-cover rounded-xl"
+              src="/images/second.webp"
+              width="100%"
+            />
+            {/* Transparent Overlay */}
+            <div className="absolute bottom-0 left-0 right-0 bg-white/20 dark:bg-black/20 backdrop-blur-md rounded-b-xl py-4 px-4 flex justify-between items-center">
+              <Tooltip content="Available soon" placement="top" color="foreground">
+                <p className="text-tiny text-default-500 cursor-pointer">More Info</p>
+              </Tooltip>
+            </div>
+          </CardBody>
+        </Card>
+
+        {/* Third Card (Basic Offer) */}
+        <Card 
+          className="py-4 shadow-lg bg-white dark:bg-gray-800 rounded-lg w-full lg:w-[30%] lg:h-[450px] cursor-pointer lg:order-3"
+          as="a" href="https://buy.stripe.com/6oE7t485MgY5doI3ci"
+        >
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <p className="text-tiny uppercase font-bold text-gray-900 dark:text-gray-200">Basic Offer</p>
+            <small className="text-default-500 dark:text-gray-400">6 Months Protection</small>
+            <h4 className="font-bold text-large text-gray-900 dark:text-gray-200">Cicada VPN - 6 Months Plan</h4>
+            <p className="text-default-500 text-sm mt-2 dark:text-gray-400">6.99 €/mo for 6 months of VPN protection.</p>
+          </CardHeader>
+
+          {/* Image Section with More Info */}
+          <CardBody className="relative py-2 px-4">
+            <Image
+              alt="Basic Offer"
+              className="object-cover rounded-xl"
+              src="/images/third.webp"
+              width="100%"
+            />
+            {/* Transparent Overlay */}
+            <div className="absolute bottom-0 left-0 right-0 bg-white/20 dark:bg-black/20 backdrop-blur-md rounded-b-xl py-4 px-4 flex justify-between items-center">
+              <Tooltip content="Available soon" placement="top" color="foreground">
+                <p className="text-tiny text-default-500 cursor-pointer">More Info</p>
+              </Tooltip>
+            </div>
+          </CardBody>
+        </Card>
+
+      </section>
+
+      <div className="text-center text-gray-500 dark:text-gray-400 mt-10 text-sm">
+        *All amounts shown are in Euros. Taxes may apply based on your jurisdiction.
+      </div>
+
+      <div className="text-center text-black dark:text-white mt-12 text-2xl font-bold">
+        See What’s Included in All Plans
+      </div>
+
+      {/* Accordion Section */}
+      <div className="mt-8">
+        <Accordion variant="shadow">
+          <AccordionItem key="1" aria-label="Accordion 1" title="Plan Benefits">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="2" aria-label="Accordion 2" title="Privacy Protection">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="3" aria-label="Accordion 3" title="Global Access">
+            {defaultContent}
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </main>
   );
 }
--- apps/docs/next-sitemap.config.js
+++ apps/docs/next-sitemap.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://nextui.org",
-  generateRobotsTxt: true,
-  exclude: ["/examples/*"],
+  siteUrl: "https://cicadavpn.com",  // Replace with your actual domain
+  generateRobotsTxt: true,  // Generates a robots.txt file
+  exclude: ["/admin/*", "/private/*"],  // Add any routes you want to exclude from the sitemap
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "*", disallow: ["/admin", "/private"] },
+    ],
+  },
 };
